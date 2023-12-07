14e821e8c43c84bff4516bbae539fd1a78782a80
olavolicursi <licursiolavo@gmail.com>
2023-12-07 19:36:50 -0300

    serializer

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
 from genres.models import Genre
 from rest_framework import generics
+from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
-    serializer_class = None
+    serializer_class = GenreSerializer
 
 # def genre_view(request):
 #     genres = Genre.objects.all()

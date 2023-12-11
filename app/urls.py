e8615f8618bdaa7074c594f28a9c58bee07117dc
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2023-12-11 11:03:22 -0300

    CRUD movie

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -26,6 +27,9 @@ urlpatterns = [
     path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
     path('genres/<int:pk>', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 
-    path('actors/', ActorCreateListView.as_view(), name='acotr-create-list'),
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
     path('actors/<int:pk>', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-create-list'),
 ]

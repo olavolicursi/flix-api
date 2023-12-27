5e12d8388e7d5b0c6b18825e0f0d2f0f7ead4eab
olavolicursi <licursiolavo@gmail.com>
2023-12-27 18:03:56 -0300

    genres permissions

--- genres/views.py
+++ genres/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from genres.models import Genre
 from genres.serializers import GenreSerializer
+from .permissions import GenrePermissionClass
 
 
 class GenreCreateListView(generics.ListCreateAPIView):

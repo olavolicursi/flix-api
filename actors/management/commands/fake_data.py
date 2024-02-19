6ee9992e7ecd4a156c029706ffcb94839c139f28
olavolicursi <69656251+olavolicursi@users.noreply.github.com>
2024-02-19 10:42:30 -0300

    commands

---
+++ actors/management/commands/fake_data.py
@@ -0,0 +1,30 @@
+import csv
+from django.core.management.base import BaseCommand
+from faker import Faker
+import random
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Configuração do Faker
+        fake = Faker()
+
+        # Configuração do arquivo CSV
+        csv_filename = 'dados_fake.csv'
+        fieldnames = ['name', 'birthday', 'nationality']
+        num_registros = 20  # Escolha o número desejado de registros
+
+        # Gera dados fake e escreve no arquivo CSV
+        with open(csv_filename, 'w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for _ in range(num_registros):
+                name = fake.name()
+                birthday = fake.date_of_birth().strftime('%Y-%m-%d')
+                nationality = fake.country()
+
+                writer.writerow({'name': name, 'birthday': birthday, 'nationality': nationality})
+
+        print(f'Arquivo CSV "{csv_filename}" gerado com sucesso.')
